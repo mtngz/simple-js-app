2b75e2c5b9ec726fc61fbea8e98acf6b84ba7297
mtngz <70101199+mtngz@users.noreply.github.com>
2020-09-12 16:53:27 +0200

    Update "," to replace ";" in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-  {name: "Venusaur", height: 2, weight: 100, types: ["grass", "poison"]};
-  {name: "Charizard", height: 1.7, weight: 90.5, types: ["fire", "flying"]};
-  {name: "Blastoise", height: 1.6, weight: 85.5, types: ["water"]};
-  {name: "Raichu", height: 0.8, weight: 30, types: ["electric"]};
+  {name: "Venusaur", height: 2, weight: 100, types: ["grass", "poison"]},
+  {name: "Charizard", height: 1.7, weight: 90.5, types: ["fire", "flying"]},
+  {name: "Blastoise", height: 1.6, weight: 85.5, types: ["water"]},
+  {name: "Raichu", height: 0.8, weight: 30, types: ["electric"]},
 ];
