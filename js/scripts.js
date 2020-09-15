0d1ebfe589954da8edf2d002c398ba309aba46e0
mtngz <70101199+mtngz@users.noreply.github.com>
2020-09-15 11:22:06 +0200

    create a for-loop iterating over pokemonList in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,7 @@ let pokemonList = [
   {name: "Blastoise", height: 1.6, weight: 85.5, types: ["water"]},
   {name: "Raichu", height: 0.8, weight: 30, types: ["electric"]},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write("<p>" + pokemonList[i].name + " (length: " + pokemonList[i].height + ")</p>" );
+}
