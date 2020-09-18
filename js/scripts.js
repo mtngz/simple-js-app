c0cef23ec59fde13e6bc1a516aaeaeebfa129563
mtngz <70101199+mtngz@users.noreply.github.com>
2020-09-18 18:30:40 +0200

    wrap pokemonList in IIFE in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-let pokemonRepository = (function () {})();
-
-let pokemonList = [
-  { name: "Venusaur", height: 2, weight: 100, types: ["grass", "poison"] },
-  { name: "Charizard", height: 1.7, weight: 90.5, types: ["fire", "flying"] },
-  { name: "Blastoise", height: 1.6, weight: 85.5, types: ["water"] },
-  { name: "Raichu", height: 0.8, weight: 30, types: ["electric"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Venusaur", height: 2, weight: 100, types: ["grass", "poison"] },
+    { name: "Charizard", height: 1.7, weight: 90.5, types: ["fire", "flying"] },
+    { name: "Blastoise", height: 1.6, weight: 85.5, types: ["water"] },
+    { name: "Raichu", height: 0.8, weight: 30, types: ["electric"] },
+  ];
+})();
 
 // for (let i = 0; i < pokemonList.length; i++) {
 //   if (pokemonList[i].height >= 2) {
