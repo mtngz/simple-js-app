eadc28adcbf77e2f8de8bebd5210c5405f437122
mtngz <70101199+mtngz@users.noreply.github.com>
2020-09-23 17:39:25 +0200

    update function showDetails with loadDetails.then in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,9 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon.name);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
 
   function loadList() {
