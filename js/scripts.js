0f3e01055e43600aab2d6693fd223859194a5bda
mtngz <70101199+mtngz@users.noreply.github.com>
2020-09-24 19:24:58 +0200

    add modalContainer in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  let modalContainer = document.querySelector("#modal-container");
 
   function add(pokemon) {
     if (typeof pokemon === "object" && "name" in pokemon) {
